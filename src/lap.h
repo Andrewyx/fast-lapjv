402bed90186efb5ffe5d1b361c4e1d24676d91fc
Andrew Mao <andrewyxmao@gmail.com>
2026-03-12 23:03:36 -0700

    refactor

--- src/lap.h
+++ src/lap.h
@@ -23,19 +23,13 @@
 #include "matrix.h"
 #include <algorithm> // Needed for std::max
 #include <cmath>     // Needed for std::abs
+#include <vector>
 
 /*************** TYPES      *******************/
 
 typedef int row;
 typedef int col;
 typedef double cost;
-
-struct MatrixPosition
-{
-    row i;
-    col j;
-};
-
 
 template <typename Data>
 class LAPJV
@@ -52,248 +46,295 @@ public:
         // LAPJV requires a square matrix. Pad to the largest dimension.
         int dim = std::max(original_rows, original_cols);
 
-
         // Helper lambda to seamlessly provide 0-cost padding for out-of-bounds access
         auto cost_at = [&](int r, int c) -> Data {
             // 0 cost ensures "dummy" assignments don't penalize the real assignments
             return r < original_rows && c < original_cols ? m(r, c) : 0;
         };
 
-        bool unassignedfound;
+        // row_assignment[i] = column assigned to row i
+        std::vector<col> row_assignment(dim, 0);
+        // col_assignment[j] = row assigned to column j, or -1 if unassigned
+        std::vector<row> col_assignment(dim, 0);
+        
+        // Dual variables for rows (u) and columns (v)
+        std::vector<cost> dual_u(dim, 0);
+        std::vector<cost> dual_v(dim, 0);
 
-        row numfree = 0, prvnumfree, f, i0, k, freerow, *pred, *freeunassigned;
-        col j1, j2, endofpath, last, low, up, *collist, *matches;
-        cost min, h, umin, usubmin, v2, *d;
-
-        col* row_solutions = new col[dim];
-        row* col_solutions = new row[dim];
-        cost* u = new cost[dim];
-        cost* v = new cost[dim];
-
-        freeunassigned = new row[dim];
-        collist = new col[dim];
-        matches = new col[dim];
-        d = new cost[dim];
-        pred = new row[dim];
-
-        MatrixPosition pos_;
-        for (pos_.i = 0; pos_.i < dim; pos_.i++) matches[pos_.i] = 0;
+        std::vector<row> unassigned_rows(dim, 0);
+        
+        // Structures used for shortest path calculations (Dijkstra)
+        std::vector<col> path_cols(dim, 0);
+        std::vector<col> col_match_counts(dim, 0);
+        std::vector<cost> shortest_path_costs(dim, 0);
+        std::vector<row> predecessor_rows(dim, 0);
 
         auto column_reduction = [&]() -> cost {
-            row imin = 0;
-            cost min = 0;
-            // COLUMN REDUCTION
-            for (pos_.j = dim; pos_.j--;)
+            row min_row = 0;
+            cost current_min_cost = 0;
+            
+            // STEP 1: COLUMN REDUCTION
+            // For each column, find the minimum cost and subtract it from all elements in the column.
+            for (col j = dim; j--;)
             {
-                min = cost_at(0, pos_.j);
-                imin = 0;
-                for (pos_.i = 1; pos_.i < dim; pos_.i++)
+                current_min_cost = cost_at(0, j);
+                min_row = 0;
+                for (row i = 1; i < dim; i++)
                 {
-                    if (cost_at(pos_.i, pos_.j) < min)
+                    if (cost_at(i, j) < current_min_cost)
                     {
-                        min = cost_at(pos_.i, pos_.j);
-                        imin = pos_.i;
+                        current_min_cost = cost_at(i, j);
+                        min_row = i;
                     }
                 }
-                v[pos_.j] = min;
+                dual_v[j] = current_min_cost;
 
-                if (++matches[imin] == 1)
+                // Track the number of times a row contains the minimum of a column
+                if (++col_match_counts[min_row] == 1)
                 {
-                    row_solutions[imin] = pos_.j;
-                    col_solutions[pos_.j] = imin;
+                    // First time we see this row as a minimum, tentatively assign it
+                    row_assignment[min_row] = j;
+                    col_assignment[j] = min_row;
                 }
-                else if (v[pos_.j] < v[row_solutions[imin]])
+                else if (dual_v[j] < dual_v[row_assignment[min_row]])
                 {
-                    int j1 = row_solutions[imin];
-                    row_solutions[imin] = pos_.j;
-                    col_solutions[pos_.j] = imin;
-                    col_solutions[j1] = -1;
+                    // If the row was already assigned, but this column has a lower minimum cost,
+                    // reassign the row to this new column.
+                    int prev_j = row_assignment[min_row];
+                    row_assignment[min_row] = j;
+                    col_assignment[j] = min_row;
+                    col_assignment[prev_j] = -1;
                 }
                 else
                 {
-                    col_solutions[pos_.j] = -1;
+                    // Otherwise, this column remains unassigned for now.
+                    col_assignment[j] = -1;
                 }
             }
-            return min;
+            return current_min_cost;
         };
 
-
-        min = column_reduction();
-        // REDUCTION TRANSFER
-        for (pos_.i = 0; pos_.i < dim; pos_.i++)
+        cost min_reduced_cost = column_reduction();
+        row num_unassigned_rows = 0;
+        
+        // STEP 2: REDUCTION TRANSFER
+        // Transfer the reduction from rows that have exactly one assigned column.
+        for (row i = 0; i < dim; i++)
         {
-            if (matches[pos_.i] == 0)
-                freeunassigned[numfree++] = pos_.i;
-            else if (matches[pos_.i] == 1)
+            if (col_match_counts[i] == 0)
             {
-                j1 = row_solutions[pos_.i];
-                min = BIG;
-                for (pos_.j = 0; pos_.j < dim; pos_.j++)
-                    if (pos_.j != j1)
-                        if (cost_at(pos_.i, pos_.j) - v[pos_.j] < min) min = cost_at(pos_.i, pos_.j) - v[pos_.j];
-                v[j1] = v[j1] - min;
+                // Row has no assignments
+                unassigned_rows[num_unassigned_rows++] = i;
+            }
+            else if (col_match_counts[i] == 1)
+            {
+                // Row is uniquely assigned to a column
+                col j1 = row_assignment[i];
+                min_reduced_cost = BIG;
+                for (col j = 0; j < dim; j++)
+                {
+                    if (j != j1)
+                    {
+                        if (cost_at(i, j) - dual_v[j] < min_reduced_cost)
+                        {
+                            min_reduced_cost = cost_at(i, j) - dual_v[j];
+                        }
+                    }
+                }
+                dual_v[j1] = dual_v[j1] - min_reduced_cost;
             }
         }
 
-        // AUGMENTING ROW REDUCTION
+        // STEP 3: AUGMENTING ROW REDUCTION
+        // Further reduce costs by examining unassigned rows.
         int loopcnt = 0;
         do
         {
             loopcnt++;
 
-            k = 0;
-            prvnumfree = numfree;
-            numfree = 0;
-            while (k < prvnumfree)
+            row k = 0;
+            row prev_num_unassigned = num_unassigned_rows;
+            num_unassigned_rows = 0;
+            while (k < prev_num_unassigned)
             {
-                pos_.i = freeunassigned[k];
+                row i = unassigned_rows[k];
                 k++;
 
-                umin = cost_at(pos_.i, 0) - v[0];
-                j1 = 0;
-                usubmin = BIG;
-                for (pos_.j = 1; pos_.j < dim; pos_.j++)
+                // Find the two smallest reduced costs in row i
+                cost min_cost = cost_at(i, 0) - dual_v[0];
+                col best_col = 0;
+                cost second_min_cost = BIG;
+                col second_best_col = 0;
+                
+                for (col j = 1; j < dim; j++)
                 {
-                    h = cost_at(pos_.i, pos_.j) - v[pos_.j];
-                    if (h < usubmin)
-                        if (h >= umin)
+                    cost reduced_cost = cost_at(i, j) - dual_v[j];
+                    if (reduced_cost < second_min_cost)
+                    {
+                        if (reduced_cost >= min_cost)
                         {
-                            usubmin = h;
-                            j2 = pos_.j;
+                            second_min_cost = reduced_cost;
+                            second_best_col = j;
                         }
                         else
                         {
-                            usubmin = umin;
-                            umin = h;
-                            j2 = j1;
-                            j1 = pos_.j;
+                            second_min_cost = min_cost;
+                            min_cost = reduced_cost;
+                            second_best_col = best_col;
+                            best_col = j;
                         }
+                    }
                 }
 
-                i0 = col_solutions[j1];
-                if (usubmin - umin > EPSILON)
-                    v[j1] = v[j1] - (usubmin - umin);
-                else if (i0 > -1)
+                row previously_assigned_row = col_assignment[best_col];
+                if (second_min_cost - min_cost > EPSILON)
                 {
-                    j1 = j2;
-                    i0 = col_solutions[j2];
+                    dual_v[best_col] = dual_v[best_col] - (second_min_cost - min_cost);
+                }
+                else if (previously_assigned_row > -1)
+                {
+                    best_col = second_best_col;
+                    previously_assigned_row = col_assignment[second_best_col];
                 }
 
-                row_solutions[pos_.i] = j1;
-                col_solutions[j1] = pos_.i;
+                // Assign row i to the best column found
+                row_assignment[i] = best_col;
+                col_assignment[best_col] = i;
 
-                if (i0 > -1)
-                    if (usubmin - umin > EPSILON)
-                        freeunassigned[--k] = i0;
+                if (previously_assigned_row > -1)
+                {
+                    // The column was already assigned to another row.
+                    // We displaced it, so add the displaced row back to the unassigned list.
+                    if (second_min_cost - min_cost > EPSILON)
+                    {
+                        unassigned_rows[--k] = previously_assigned_row;
+                    }
                     else
-                        freeunassigned[numfree++] = i0;
+                    {
+                        unassigned_rows[num_unassigned_rows++] = previously_assigned_row;
+                    }
+                }
             }
         }
         while (loopcnt < 2);
 
-        // AUGMENT SOLUTION for each free row.
-        for (f = 0; f < numfree; f++)
+        // STEP 4: AUGMENT SOLUTION
+        // For each remaining unassigned row, find an augmenting path to increase assignments.
+        // This is effectively Dijkstra's algorithm for finding the shortest alternating path.
+        for (row f = 0; f < num_unassigned_rows; f++)
         {
-            freerow = freeunassigned[f];
+            row current_free_row = unassigned_rows[f];
 
-            for (pos_.j = dim; pos_.j--;)
+            // Initialize shortest path costs
+            for (col j = dim; j--;)
             {
-                d[pos_.j] = cost_at(freerow, pos_.j) - v[pos_.j];
-                pred[pos_.j] = freerow;
-                collist[pos_.j] = pos_.j;
+                shortest_path_costs[j] = cost_at(current_free_row, j) - dual_v[j];
+                predecessor_rows[j] = current_free_row;
+                path_cols[j] = j;
             }
 
-            low = 0;
-            up = 0;
-            unassignedfound = false;
+            col search_head = 0;
+            col search_tail = 0;
+            bool unassigned_found = false;
+            col end_of_path = 0;
+            col last_processed = 0;
+
             do
             {
-                if (up == low)
+                if (search_tail == search_head)
                 {
-                    last = low - 1;
+                    last_processed = search_head - 1;
 
-                    min = d[collist[up++]];
-                    for (k = up; k < dim; k++)
+                    min_reduced_cost = shortest_path_costs[path_cols[search_tail++]];
+                    for (col k = search_tail; k < dim; k++)
                     {
-                        pos_.j = collist[k];
-                        h = d[pos_.j];
-                        if (h <= min + EPSILON)
+                        col j = path_cols[k];
+                        cost current_cost = shortest_path_costs[j];
+                        if (current_cost <= min_reduced_cost + EPSILON)
                         {
-                            if (h < min - EPSILON)
+                            if (current_cost < min_reduced_cost - EPSILON)
                             {
-                                up = low;
-                                min = h;
+                                search_tail = search_head;
+                                min_reduced_cost = current_cost;
                             }
-                            collist[k] = collist[up];
-                            collist[up++] = pos_.j;
+                            path_cols[k] = path_cols[search_tail];
+                            path_cols[search_tail++] = j;
                         }
                     }
-                    for (k = low; k < up; k++)
-                        if (col_solutions[collist[k]] < 0)
+                    for (col k = search_head; k < search_tail; k++)
+                    {
+                        if (col_assignment[path_cols[k]] < 0)
                         {
-                            endofpath = collist[k];
-                            unassignedfound = true;
+                            end_of_path = path_cols[k];
+                            unassigned_found = true;
                             break;
                         }
+                    }
                 }
 
-                if (!unassignedfound)
+                if (!unassigned_found)
                 {
-                    j1 = collist[low];
-                    low++;
-                    pos_.i = col_solutions[j1];
-                    h = cost_at(pos_.i, j1) - v[j1] - min;
+                    col next_col_to_process = path_cols[search_head];
+                    search_head++;
+                    row assigned_row = col_assignment[next_col_to_process];
+                    cost current_cost_margin = cost_at(assigned_row, next_col_to_process) - dual_v[next_col_to_process] - min_reduced_cost;
 
-                    for (k = up; k < dim; k++)
+                    for (col k = search_tail; k < dim; k++)
                     {
-                        pos_.j = collist[k];
-                        v2 = cost_at(pos_.i, pos_.j) - v[pos_.j] - h;
-                        if (v2 < d[pos_.j])
+                        col j = path_cols[k];
+                        cost new_cost = cost_at(assigned_row, j) - dual_v[j] - current_cost_margin;
+                        if (new_cost < shortest_path_costs[j])
                         {
-                            pred[pos_.j] = pos_.i;
-                            if (std::abs(v2 - min) <= EPSILON)
-                                if (col_solutions[pos_.j] < 0)
+                            predecessor_rows[j] = assigned_row;
+                            if (std::abs(new_cost - min_reduced_cost) <= EPSILON)
+                            {
+                                if (col_assignment[j] < 0)
                                 {
-                                    endofpath = pos_.j;
-                                    unassignedfound = true;
+                                    end_of_path = j;
+                                    unassigned_found = true;
                                     break;
                                 }
                                 else
                                 {
-                                    collist[k] = collist[up];
-                                    collist[up++] = pos_.j;
+                                    path_cols[k] = path_cols[search_tail];
+                                    path_cols[search_tail++] = j;
                                 }
-                            d[pos_.j] = v2;
+                            }
+                            shortest_path_costs[j] = new_cost;
                         }
                     }
                 }
             }
-            while (!unassignedfound);
+            while (!unassigned_found);
 
-            for (k = last + 1; k--;)
+            // Update dual variables along the alternating path
+            for (col k = last_processed + 1; k--;)
             {
-                j1 = collist[k];
-                v[j1] = v[j1] + d[j1] - min;
+                col j1 = path_cols[k];
+                dual_v[j1] = dual_v[j1] + shortest_path_costs[j1] - min_reduced_cost;
             }
 
+            // Augment the current matching
+            row i;
+            col j1;
             do
             {
-                pos_.i = pred[endofpath];
-                col_solutions[endofpath] = pos_.i;
-                j1 = endofpath;
-                endofpath = row_solutions[pos_.i];
-                row_solutions[pos_.i] = j1;
+                i = predecessor_rows[end_of_path];
+                col_assignment[end_of_path] = i;
+                j1 = end_of_path;
+                end_of_path = row_assignment[i];
+                row_assignment[i] = j1;
             }
-            while (pos_.i != freerow);
+            while (i != current_free_row);
         }
 
-        // calculate optimal cost.
+        // STEP 5: CALCULATE OPTIMAL COST & FINALIZE
         cost lapcost = 0;
-        for (pos_.i = dim; pos_.i--;)
+        for (row i = dim; i--;)
         {
-            pos_.j = row_solutions[pos_.i];
-            u[pos_.i] = cost_at(pos_.i, pos_.j) - v[pos_.j];
-            lapcost = lapcost + cost_at(pos_.i, pos_.j);
+            col j = row_assignment[i];
+            dual_u[i] = cost_at(i, j) - dual_v[j];
+            lapcost = lapcost + cost_at(i, j);
         }
 
         // Write assignments back into the input matrix
@@ -301,7 +342,7 @@ public:
         {
             for (size_t c = 0; c < m.columns(); ++c)
             {
-                if (row_solutions[r] == (col)c)
+                if (row_assignment[r] == (col)c)
                 {
                     m(r, c) = 0.0;
                 }
@@ -311,19 +352,7 @@ public:
                 }
             }
         }
-
-        // free reserved memory.
-        delete[] pred;
-        delete[] freeunassigned;
-        delete[] collist;
-        delete[] matches;
-        delete[] d;
-
-        delete[] row_solutions;
-        delete[] col_solutions;
-        delete[] u;
-        delete[] v;
-    };
+    }
 
 private:
 };
